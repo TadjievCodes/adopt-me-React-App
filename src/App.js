7a8208bb57f8c8b2605a91f8d6b68d640871bee3
TadjievCodes <muhammad2008.08@mail.ru>
2021-06-22 01:02:06 -0400

    Some changes, eslint installed, converted to jsx

--- src/App.js
+++ src/App.js
@@ -1,55 +1,33 @@
- import React from 'react';
- import XReactDOM from 'react-dom';
-
-
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet"
 // your code goes here for now
-    const Pet = ({ name, animal, breed })  => {
-        return React.createElement( "div",  {}, 
-        [
-            React.createElement("h1", {}, name),
-            React.createElement("h2", {}, animal),
-            React.createElement("h2", {}, breed),
-            
-         ] );
-    };
 
-  
+// React.createElement gets three parameters: first> type of element it is, second> object, third> the children it's creating
 
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cokatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "mixed",
+    }),
+  ]);
+};
 
-      // React.createElement gets three parameters: first> type of element it is, second> object, third> the children it's creating 
-  
-      const App = () => {
-           return React.createElement(
-              "div",
-              {id: "something-important"},
-              [
-                React.createElement("h1", {}, "Adopt Me!"),
-                React.createElement(Pet, {
-                name: "Luna", 
-                animal: "Dog",
-                breed: "Havanese"
-                }),
-                React.createElement(Pet, {
-                name: "Pepper", 
-                animal: "Bird", 
-                breed: "Cokatiel"
-                }),
-                React.createElement(Pet, {
-                name: "Doink",
-                animal: "Cat",
-                breed: "mixed"
-                })
-
-              ]      
-           );
-      };
-    
-
-          
-
-     ReactDOM.render(
-     //<App />,
-     React.createElement(App), 
-     document.getElementById("root") 
-     );
-
+render(
+  //<App />,
+  React.createElement(App),
+  document.getElementById("root")
+);
