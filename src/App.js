d32760df498b209f285d5a271fd9d4c5c50d39a3
TadjievCodes <muhammad2008.08@mail.ru>
2021-07-01 00:49:13 -0400

    Created  a SearchParams component input

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet"
+import SearchParams from "./SearchParams";
+import "./style.css";
+
 // your code goes here for now
 
 // React.createElement gets three parameters: first> type of element it is, second> object, third> the children it's creating
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cokatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "mixed",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(
-  //<App />,
-  React.createElement(App),
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
