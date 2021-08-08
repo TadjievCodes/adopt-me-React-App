dd80ce397a29f8a2174112861a176e4af0449916
TadjievCodes <muhammad2008.08@mail.ru>
2021-08-07 23:08:07 -0400

    Done with useEffect for Breeds

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,20 +4,19 @@ import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
   useEffect(() => {
     setBreeds([]);
     setBreed("");
 
-    pet.breeds("animal").then(({ breeds }) => {
-      const breedStrings = breeds.map(({ name }) => name);
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
-  });
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
